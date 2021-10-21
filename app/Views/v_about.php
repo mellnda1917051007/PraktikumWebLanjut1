fba1e6f7669ca57f4ec23ba76425eff8a8781e19
mellnda1917051007 <smelinda140@gmail.com>
2021-10-21 20:18:49 +0700

    web lanjut commit

---
+++ app/Views/v_about.php
@@ -0,0 +1,7 @@
+<div class="container mt-3">
+    <div class="text-center">
+        <img src="/assets/images/Linux.jpeg" alt="logo" width="180" class="img-thumbnail rounded-circle">
+        <h3 class="display-4">Praktikum Web Lanjut Dengan Studi Kasus Blog App</h3>
+        <p class="lead">Programming | Codeigniter 4 | Php</p>
+    </div>
+</div>
